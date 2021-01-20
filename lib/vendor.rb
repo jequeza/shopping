3ce353be8e534187d9f6d7af163ea51809211e87
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-20 10:29:15 -0600

    Add #potential_revenue to Vendor class, all tests pass.

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -14,4 +14,10 @@ class Vendor
   def stock(item_obj, amount)
     @inventory[item_obj] += amount
   end
+
+  def potential_revenue
+    @inventory.sum do |item, amount|
+      item.price * amount
+    end
+  end
 end
