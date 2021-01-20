d097a825576b573f7d044df2257d088bf886dc93
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-20 10:20:41 -0600

    Add test for stock and check stock functionality of Vendor class.

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -16,4 +16,17 @@ class VendorTest < Minitest::Test
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     assert_equal 0, vendor.check_stock(item1)
   end
+
+  def test_it_can_stock_items
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor.stock(item1, 30)
+    assert_equal ({item1 => 30}), vendor.inventory
+    assert_equal 30, vendor.check_stock(item1)
+    vendor.stock(item1, 25)
+    assert_equal 55, vendor.check_stock(item1)
+    vendor.stock(item2, 12)
+    assert_equal ({item1 => 55, item2 => 12}), vendor.inventory
+  end
 end
