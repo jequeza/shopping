a8adfecaefc01780aff6b61c62fdf889683a966a
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-01-20 10:14:46 -0600

    Add test for #check_stock

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -8,6 +8,12 @@ class VendorTest < Minitest::Test
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_instance_of Vendor, vendor
     assert_equal "Rocky Mountain Fresh", vendor.name
-    assert_equal ({}), vendor.stock
+    assert_equal ({}), vendor.inventory
+  end
+
+  def test_it_has_0_stock_of_an_item_by_default
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    assert_equal 0, vendor.check_stock(item1)
   end
 end
